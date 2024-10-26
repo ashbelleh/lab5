ef43620681402924d5d4f1a07238423a4711e8b8
ashbelleh <mhoun012@uottawa.ca>
2024-10-26 11:27:22 -0400

    lab05

--- app/src/main/java/com/example/lab5/MainActivity.java
+++ app/src/main/java/com/example/lab5/MainActivity.java
@@ -1,7 +1,20 @@
 package com.example.lab5;
 
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.Bundle;
 
@@ -21,6 +34,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextName;
@@ -29,11 +44,15 @@ public class MainActivity extends AppCompatActivity {
     ListView listViewProducts;
 
     List<Product> products;
+    DatabaseReference databaseProducts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
 
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextPrice = (EditText) findViewById(R.id.editTextPrice);
@@ -60,62 +79,119 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     @Override
     protected void onStart() {
         super.onStart();
-    }
-
-
-    private void showUpdateDeleteDialog(final String productId, String productName) {
-
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        LayoutInflater inflater = getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
-        dialogBuilder.setView(dialogView);
-
-        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
-        final EditText editTextPrice  = (EditText) dialogView.findViewById(R.id.editTextPrice);
-        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
-        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
-
-        dialogBuilder.setTitle(productName);
-        final AlertDialog b = dialogBuilder.create();
-        b.show();
-
-        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+        databaseProducts.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                String name = editTextName.getText().toString().trim();
-                double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
-                if (!TextUtils.isEmpty(name)) {
-                    updateProduct(productId, name, price);
+            public void onDataChange (DataSnapshot dataSnapshot) {
+                //clearing the previous artist list
+                products.clear();
+
+                //iterating through all the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    //getting product
+                    Product product = postSnapshot.getValue(Product.class) ;
+
+                    if (product != null) {
+                    //adding product to the list
+                    products.add (product) ;
+                    }
+                }
+
+                    //creating adapter
+                    ProductList productsAdapter = new ProductList (MainActivity.this, products);
+                    //attaching adapter to the listview
+                    listViewProducts.setAdapter (productsAdapter);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Failed to load Products", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+
+        private void showUpdateDeleteDialog(final String productId, String productName) {
+
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+            dialogBuilder.setView(dialogView);
+
+            final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+            final EditText editTextPrice  = (EditText) dialogView.findViewById(R.id.editTextPrice);
+            final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
+            final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
+
+            dialogBuilder.setTitle(productName);
+            final AlertDialog b = dialogBuilder.create();
+            b.show();
+
+            buttonUpdate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String name = editTextName.getText().toString().trim();
+                    double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+                    if (!TextUtils.isEmpty(name)) {
+                        updateProduct(productId, name, price);
+                        b.dismiss();
+                    }
+                }
+            });
+
+            buttonDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    deleteProduct(productId);
                     b.dismiss();
                 }
+            });
+        }
+
+        private void updateProduct(String id, String name, double price) {
+            //getting the specified product reference
+            DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+            //updating product
+            Product product = new Product(id, name, price);
+            dR.setValue(product);
+            Toast.makeText(getApplicationContext(), "Product Updated", Toast.LENGTH_LONG).show();
+        }
+
+        private void deleteProduct(String id) {
+
+            //getting the specified product reference
+            DatabaseReference dR = FirebaseDatabase.getInstance().getReference("products").child(id);
+            //removing prodct
+            dR. removeValue();
+            Toast.makeText(getApplicationContext(),"Product Deleted", Toast.LENGTH_LONG) .show();
+        }
+
+        private void addProduct() {
+            //getting the values to save
+            String name = editTextName.getText().toString().trim();
+            double price = Double.parseDouble(String.valueOf(editTextPrice.getText().toString()));
+            //checking if the value is provided
+
+            if (!TextUtils.isEmpty(name)) {
+                //getting a unique id using push().getKey();
+                //it will create a unique id and we will use it as the Primary Key for our Product
+                String id = databaseProducts.push().getKey();
+                //creating a product object
+                Product product = new Product(id, name, price);
+                //Saving the product
+                databaseProducts.child(id).setValue(product);
+                //setting edittext to blank again
+                editTextName.setText("");
+                editTextPrice.setText("");
+                //displaying a success toast
+                Toast.makeText (this, "Product added", Toast. LENGTH_LONG).show();
+
+            } else {
+                //if the value is not given displaying a toast
+                Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
             }
-        });
+        }
 
-        buttonDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                deleteProduct(productId);
-                b.dismiss();
-            }
-        });
     }
-
-    private void updateProduct(String id, String name, double price) {
-
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
-    }
-
-    private void deleteProduct(String id) {
-
-        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
-    }
-
-    private void addProduct() {
-
-        Toast.makeText(this, "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
-    }
-}
